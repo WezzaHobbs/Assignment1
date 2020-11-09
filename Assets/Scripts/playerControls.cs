c1bbd9e86b003b6d4b466e96857b8cab3cd29ce8
WezzaHobbs <WezzaHobbs@users.noreply.github.com>
2020-11-09 16:17:57 +0000

    09/11/2020 Pt.2

---
+++ Assets/Scripts/playerControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<RigidBody2D>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
