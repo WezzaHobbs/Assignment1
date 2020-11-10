06e7eeec31cdbbde9bac8fa1fee54652947c7472
WezzaHobbs <WezzaHobbs@users.noreply.github.com>
2020-11-10 12:16:40 +0000

    10/11/2020
    Started on code

--- Assets/Scripts/playerControls.cs
+++ Assets/Scripts/playerControls.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class playerControls : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>;
+        rb = GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        int speed = 20;
+        int ySpeed;
+        int xSpeed;
+        if(Input.GetKey("w"))
+        {
+            ySpeed = speed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            ySpeed = -speed;
+        }
+        else
+        {
+            ySpeed = 0;
+        }
+        if (Input.GetKey("d"))
+        {
+            xSpeed = speed;
+        }
+        else if (Input.GetKey("a"))
+        {
+            xSpeed = -speed;
+        }
+        else
+        {
+            xSpeed = 0;
+        }
+        if ((ySpeed != 0) && (xSpeed != 0))
+        {
+            ySpeed = Sqrt(2) / ySpeed;
+            xSpeed = Sqrt(2) / xSpeed;
+        }
+
+        rb.velocity = new Vector2(xSpeed, ySpeed);
     }
 }
